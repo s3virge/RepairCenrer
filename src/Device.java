4363258ab6048ef43cf8f4c5f6c187454e2d230e
s3virge <kobzarvit@gmail.com>
2018-10-20 22:29:23 +0300

    was created Device class

---
+++ src/Device.java
@@ -0,0 +1,57 @@
+public class Device {
+
+    //    private Integer id;
+    private String brand;
+    private String type;
+    private String model;
+    private String serialNumber;
+    private String problem;
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public String getProblem() {
+        return problem;
+    }
+
+    public void setProblem(String problem) {
+        this.problem = problem;
+    }
+
+    public Device(String brand, String type, String model, String serialNumber, String problem) {
+        this.brand = brand;
+        this.type = type;
+        this.model = model;
+        this.serialNumber = serialNumber;
+        this.problem = problem;
+    }
+}
