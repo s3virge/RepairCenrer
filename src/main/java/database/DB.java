cc30ba35329ed8d82d8e04aa0eb7e2cca092beb4
s3virge <kobzarvit@gmail.com>
2018-10-31 22:30:41 +0200

    deal with database connection

---
+++ src/main/java/database/DB.java
@@ -0,0 +1,11 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DB {
+    private Connection getConnection() {
+        Connection con = new DriverManager.getConnection("",);
+        return null;
+    }
+}
