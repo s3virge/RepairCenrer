bc3d6347b4ae3e7bc6a14503611840f32721697e
s3virge <kobzarvit@gmail.com>
2018-11-01 21:44:57 +0200

    started to deal with database

--- src/main/java/config/Config.java
+++ src/main/java/config/Config.java
@@ -1,5 +1,6 @@
 package config;
 
+import java.io.InputStream;
 import java.util.Properties;
 
 public class Config {
@@ -12,6 +13,17 @@ public class Config {
     public static String getProperty(String name) {
         if (properties == null) {
 
+            //ResourceBundle properties = ResourceBundle.getBundle("dataBase");
+
+            /*
+            * Java Classloader(завантажувач класів) є частиною
+            * Java Runtime Environment, що динамічно завантажує Java-класи в Java Virtual Machine.
+            *
+            * getResourceAsStream - Returns an input stream for reading the specified resource.*/
+            InputStream inStream = Config.class.getClassLoader()
+                    .getResourceAsStream("bd.properties");
+
+            Config.class
         }
         return properties.getProperty(name);
     }
