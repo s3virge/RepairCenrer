6e077a2e458f1cc30e8c5ffbc8dd5b6a79ae48a6
s3virge <kobzarvit@gmail.com>
2018-12-11 23:56:07 +0200

    start to create UserDao class

--- src/main/java/app/dao/UserDao.java
+++ src/main/java/app/dao/UserDao.java
@@ -1,4 +1,21 @@
 package app.dao;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * класс будет отвечать за работу с данными позьзователя
+ */
 public class UserDao {
+    private static final Logger logger = LogManager.getLogger(UserDao.class);
+
+    private static final String INSERT_USER = "";
+    private static final String SELECT_USER = "";
+
+    private Connection getConnection() throws SQLException {
+        return ConnectionBuilder.getConnection();
+    }
 }
