b0201b8b7f4990eb432a8364e3705941db0ee774
s3virge <kobzarvit@gmail.com>
2018-12-30 11:44:13 +0200

    now Cancel btn close stage

--- src/main/java/app/controllers/UserManagementDlgController.java
+++ src/main/java/app/controllers/UserManagementDlgController.java
@@ -1,4 +1,16 @@
 package app.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.stage.Stage;
+
 public class UserManagementDlgController {
+
+    @FXML
+    private void closeStage(ActionEvent event) {
+        Node source = (Node)  event.getSource();
+        Stage stage  = (Stage) source.getScene().getWindow();
+        stage.close();
+    }
 }
