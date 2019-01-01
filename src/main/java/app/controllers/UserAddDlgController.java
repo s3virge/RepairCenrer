514be20ee08b5948014538c0e0552a9f3e496151
s3virge <kobzarvit@gmail.com>
2019-01-01 13:04:50 +0200

    surname textfield get focus

--- src/main/java/app/controllers/UserAddDlgController.java
+++ src/main/java/app/controllers/UserAddDlgController.java
@@ -1,7 +1,10 @@
 package app.controllers;
 
+import app.utils.MsgBox;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -9,10 +12,13 @@ import javafx.event.ActionEvent;
 
 public class UserAddDlgController {
     private static Logger log = LogManager.getLogger(UserAddDlgController.class);
+    @FXML
+    TextField tfSurname;
 
     @FXML
     private void OnBtnAddClick() {
         log.trace("");
+        //todo save new user to database
     }
 
     @FXML
@@ -25,5 +31,12 @@ public class UserAddDlgController {
         Node source = (Node) actionEvent.getSource();
         Stage stage = (Stage) source.getScene().getWindow();
         stage.close();
+    }
+
+    @FXML
+    private void initialize() {
+//        tfSurname.requestFocus(); //did not working
+        //this trick is working
+        Platform.runLater(()->tfSurname.requestFocus());
     }
 }
