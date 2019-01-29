5840f871feae61610014df393dc69303d6f6dfab
s3virge <kobzarvit@gmail.com>
2019-01-29 11:12:33 +0200

    in status table was added new repair stage REady

--- src/main/resources/sql/createdb.sql
+++ src/main/resources/sql/createdb.sql
@@ -19,20 +19,20 @@ USE `repaircenter` ;
 -- Table `repaircenter`.`user_group`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `repaircenter`.`user_group` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `value` VARCHAR(45) NOT NULL,
+                                                         `id` INT NOT NULL AUTO_INCREMENT,
+                                                         `value` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `value_UNIQUE` (`value` ASC))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+  ENGINE = InnoDB
+  DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
 -- Table `repaircenter`.`user`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `repaircenter`.`user` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `login` VARCHAR(45) NOT NULL,
+                                                   `id` INT NOT NULL AUTO_INCREMENT,
+                                                   `login` VARCHAR(45) NOT NULL,
   `password` VARCHAR(45) NOT NULL,
   `user_group` INT NOT NULL,
   `surname` VARCHAR(45) NOT NULL,
@@ -44,159 +44,159 @@ CREATE TABLE IF NOT EXISTS `repaircenter`.`user` (
   INDEX `user_group_idx` (`user_group` ASC),
   UNIQUE INDEX `login_UNIQUE` (`login` ASC),
   CONSTRAINT `user_group`
-    FOREIGN KEY (`user_group`)
-    REFERENCES `repaircenter`.`user_group` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+  FOREIGN KEY (`user_group`)
+  REFERENCES `repaircenter`.`user_group` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION)
+  ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table `repaircenter`.`names`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `repaircenter`.`names` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `value` VARCHAR(45) NOT NULL,
+                                                    `id` INT NOT NULL AUTO_INCREMENT,
+                                                    `value` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `name_UNIQUE` (`value` ASC),
   UNIQUE INDEX `id_UNIQUE` (`id` ASC))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8
-COMMENT = 'имена пользователей';
+  ENGINE = InnoDB
+  DEFAULT CHARACTER SET = utf8
+  COMMENT = 'имена пользователей';
 
 
 -- -----------------------------------------------------
 -- Table `repaircenter`.`patronymics`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `repaircenter`.`patronymics` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `value` VARCHAR(45) NOT NULL,
+                                                          `id` INT NOT NULL AUTO_INCREMENT,
+                                                          `value` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `value_UNIQUE` (`value` ASC))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8
-COMMENT = 'отчества';
+  ENGINE = InnoDB
+  DEFAULT CHARACTER SET = utf8
+  COMMENT = 'отчества';
 
 
 -- -----------------------------------------------------
 -- Table `repaircenter`.`surnames`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `repaircenter`.`surnames` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `value` VARCHAR(45) NOT NULL,
+                                                       `id` INT NOT NULL AUTO_INCREMENT,
+                                                       `value` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `value_UNIQUE` (`value` ASC))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8
-COMMENT = 'фамилии тут';
+  ENGINE = InnoDB
+  DEFAULT CHARACTER SET = utf8
+  COMMENT = 'фамилии тут';
 
 
 -- -----------------------------------------------------
 -- Table `repaircenter`.`owner`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `repaircenter`.`owner` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `surname_id` INT NOT NULL,
-  `name_id` INT NOT NULL,
-  `patronymic_id` INT NOT NULL,
-  `phone_number` VARCHAR(15) NOT NULL,
+                                                    `id` INT NOT NULL AUTO_INCREMENT,
+                                                    `surname_id` INT NOT NULL,
+                                                    `name_id` INT NOT NULL,
+                                                    `patronymic_id` INT NOT NULL,
+                                                    `phone_number` VARCHAR(15) NOT NULL,
   `email` VARCHAR(100) NULL,
   PRIMARY KEY (`id`),
   INDEX `owner_name_idx` (`name_id` ASC),
   INDEX `owner_patronymic_idx` (`patronymic_id` ASC),
   INDEX `owner_surname_idx` (`surname_id` ASC),
   CONSTRAINT `owner_name`
-    FOREIGN KEY (`name_id`)
-    REFERENCES `repaircenter`.`names` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
+  FOREIGN KEY (`name_id`)
+  REFERENCES `repaircenter`.`names` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION,
   CONSTRAINT `owner_patronymic`
-    FOREIGN KEY (`patronymic_id`)
-    REFERENCES `repaircenter`.`patronymics` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
+  FOREIGN KEY (`patronymic_id`)
+  REFERENCES `repaircenter`.`patronymics` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION,
   CONSTRAINT `owner_surname`
-    FOREIGN KEY (`surname_id`)
-    REFERENCES `repaircenter`.`surnames` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8
-COMMENT = 'информация о пользователе';
+  FOREIGN KEY (`surname_id`)
+  REFERENCES `repaircenter`.`surnames` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION)
+  ENGINE = InnoDB
+  DEFAULT CHARACTER SET = utf8
+  COMMENT = 'информация о пользователе';
 
 
 -- -----------------------------------------------------
 -- Table `repaircenter`.`brand`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `repaircenter`.`brand` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `value` VARCHAR(45) NOT NULL,
+                                                    `id` INT NOT NULL AUTO_INCREMENT,
+                                                    `value` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `value_UNIQUE` (`value` ASC))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+  ENGINE = InnoDB
+  DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
 -- Table `repaircenter`.`type`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `repaircenter`.`type` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `value` VARCHAR(45) NOT NULL,
+                                                   `id` INT NOT NULL AUTO_INCREMENT,
+                                                   `value` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `value_UNIQUE` (`value` ASC))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8
-COMMENT = 'тип устройства';
+  ENGINE = InnoDB
+  DEFAULT CHARACTER SET = utf8
+  COMMENT = 'тип устройства';
 
 
 -- -----------------------------------------------------
 -- Table `repaircenter`.`model`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `repaircenter`.`model` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `value` VARCHAR(45) NOT NULL,
+                                                    `id` INT NOT NULL AUTO_INCREMENT,
+                                                    `value` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `value_UNIQUE` (`value` ASC))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8
-COMMENT = 'таблица для хранения модели устройства';
+  ENGINE = InnoDB
+  DEFAULT CHARACTER SET = utf8
+  COMMENT = 'таблица для хранения модели устройства';
 
 
 -- -----------------------------------------------------
 -- Table `repaircenter`.`defect`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `repaircenter`.`defect` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `value` VARCHAR(500) NOT NULL,
+                                                     `id` INT NOT NULL AUTO_INCREMENT,
+                                                     `value` VARCHAR(500) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `description_UNIQUE` (`value` ASC))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8
-COMMENT = 'неисправности';
+  ENGINE = InnoDB
+  DEFAULT CHARACTER SET = utf8
+  COMMENT = 'неисправности';
 
 
 -- -----------------------------------------------------
 -- Table `repaircenter`.`status`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `repaircenter`.`status` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `value` VARCHAR(45) NOT NULL,
+                                                     `id` INT NOT NULL AUTO_INCREMENT,
+                                                     `value` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `value_UNIQUE` (`value` ASC))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8
-COMMENT = 'этап ремонта';
+  ENGINE = InnoDB
+  DEFAULT CHARACTER SET = utf8
+  COMMENT = 'этап ремонта';
 
 
 -- -----------------------------------------------------
 -- Table `repaircenter`.`repair`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `repaircenter`.`repair` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `acceptor_id` INT NOT NULL,
-  `master_id` INT NOT NULL,
-  `master_comments` VARCHAR(1024) NULL,
+                                                     `id` INT NOT NULL AUTO_INCREMENT,
+                                                     `acceptor_id` INT NOT NULL,
+                                                     `master_id` INT NOT NULL,
+                                                     `master_comments` VARCHAR(1024) NULL,
   `diagnostic_result` VARCHAR(1024) NULL,
   `repair_result` VARCHAR(450) NULL,
   `status_id` INT NOT NULL,
@@ -209,60 +209,60 @@ CREATE TABLE IF NOT EXISTS `repaircenter`.`repair` (
   INDEX `repair_acceptor_idx` (`acceptor_id` ASC),
   INDEX `repair_master_idx` (`master_id` ASC),
   CONSTRAINT `repair_status`
-    FOREIGN KEY (`status_id`)
-    REFERENCES `repaircenter`.`status` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
+  FOREIGN KEY (`status_id`)
+  REFERENCES `repaircenter`.`status` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION,
   CONSTRAINT `repair_acceptor`
-    FOREIGN KEY (`acceptor_id`)
-    REFERENCES `repaircenter`.`user` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
+  FOREIGN KEY (`acceptor_id`)
+  REFERENCES `repaircenter`.`user` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION,
   CONSTRAINT `repair_master`
-    FOREIGN KEY (`master_id`)
-    REFERENCES `repaircenter`.`user` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8
-COMMENT = 'таблица для результатов диагностики-ремонта ';
+  FOREIGN KEY (`master_id`)
+  REFERENCES `repaircenter`.`user` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION)
+  ENGINE = InnoDB
+  DEFAULT CHARACTER SET = utf8
+  COMMENT = 'таблица для результатов диагностики-ремонта ';
 
 
 -- -----------------------------------------------------
 -- Table `repaircenter`.`completeness`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `repaircenter`.`completeness` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `value` VARCHAR(255) NOT NULL,
+                                                           `id` INT NOT NULL AUTO_INCREMENT,
+                                                           `value` VARCHAR(255) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `value_UNIQUE` (`value` ASC))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8
-COMMENT = 'комплектация устройства';
+  ENGINE = InnoDB
+  DEFAULT CHARACTER SET = utf8
+  COMMENT = 'комплектация устройства';
 
 
 -- -----------------------------------------------------
 -- Table `repaircenter`.`appearance`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `repaircenter`.`appearance` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `value` VARCHAR(500) NOT NULL,
+                                                         `id` INT NOT NULL AUTO_INCREMENT,
+                                                         `value` VARCHAR(500) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `value_UNIQUE` (`value` ASC))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8
-COMMENT = 'внешний вид устройства';
+  ENGINE = InnoDB
+  DEFAULT CHARACTER SET = utf8
+  COMMENT = 'внешний вид устройства';
 
 
 -- -----------------------------------------------------
 -- Table `repaircenter`.`device`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `repaircenter`.`device` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `type_id` INT NOT NULL,
-  `brand_id` INT NOT NULL,
-  `model_id` INT NOT NULL,
-  `serial_number` VARCHAR(45) NULL,
+                                                     `id` INT NOT NULL AUTO_INCREMENT,
+                                                     `type_id` INT NOT NULL,
+                                                     `brand_id` INT NOT NULL,
+                                                     `model_id` INT NOT NULL,
+                                                     `serial_number` VARCHAR(45) NULL,
   `defect_id` INT NOT NULL,
   `owner_id` INT NOT NULL,
   `repair_id` INT NOT NULL,
@@ -279,47 +279,47 @@ CREATE TABLE IF NOT EXISTS `repaircenter`.`device` (
   INDEX `device_completeness_idx` (`completeness_id` ASC),
   INDEX `device_appearance_idx` (`appearance_id` ASC),
   CONSTRAINT `device_owner`
-    FOREIGN KEY (`owner_id`)
-    REFERENCES `repaircenter`.`owner` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
+  FOREIGN KEY (`owner_id`)
+  REFERENCES `repaircenter`.`owner` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION,
   CONSTRAINT `device_brand`
-    FOREIGN KEY (`brand_id`)
-    REFERENCES `repaircenter`.`brand` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
+  FOREIGN KEY (`brand_id`)
+  REFERENCES `repaircenter`.`brand` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION,
   CONSTRAINT `device_type`
-    FOREIGN KEY (`type_id`)
-    REFERENCES `repaircenter`.`type` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
+  FOREIGN KEY (`type_id`)
+  REFERENCES `repaircenter`.`type` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION,
   CONSTRAINT `device_model`
-    FOREIGN KEY (`model_id`)
-    REFERENCES `repaircenter`.`model` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
+  FOREIGN KEY (`model_id`)
+  REFERENCES `repaircenter`.`model` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION,
   CONSTRAINT `device_defect`
-    FOREIGN KEY (`defect_id`)
-    REFERENCES `repaircenter`.`defect` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
+  FOREIGN KEY (`defect_id`)
+  REFERENCES `repaircenter`.`defect` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION,
   CONSTRAINT `device_repair`
-    FOREIGN KEY (`repair_id`)
-    REFERENCES `repaircenter`.`repair` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
+  FOREIGN KEY (`repair_id`)
+  REFERENCES `repaircenter`.`repair` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION,
   CONSTRAINT `device_completeness`
-    FOREIGN KEY (`completeness_id`)
-    REFERENCES `repaircenter`.`completeness` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
+  FOREIGN KEY (`completeness_id`)
+  REFERENCES `repaircenter`.`completeness` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION,
   CONSTRAINT `device_appearance`
-    FOREIGN KEY (`appearance_id`)
-    REFERENCES `repaircenter`.`appearance` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8;
+  FOREIGN KEY (`appearance_id`)
+  REFERENCES `repaircenter`.`appearance` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION)
+  ENGINE = InnoDB
+  DEFAULT CHARACTER SET = utf8;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
@@ -359,6 +359,7 @@ INSERT INTO `repaircenter`.`status` (`id`, `value`) VALUES (1, 'Принято')
 INSERT INTO `repaircenter`.`status` (`id`, `value`) VALUES (2, 'Диагностика');
 INSERT INTO `repaircenter`.`status` (`id`, `value`) VALUES (3, 'Ожидание комплектующих');
 INSERT INTO `repaircenter`.`status` (`id`, `value`) VALUES (4, 'Ремонт');
-INSERT INTO `repaircenter`.`status` (`id`, `value`) VALUES (5, 'Выдано');
+INSERT INTO `repaircenter`.`status` (`id`, `value`) VALUES (5, 'Готово');
+INSERT INTO `repaircenter`.`status` (`id`, `value`) VALUES (6, 'Выдано');
 
 COMMIT;
