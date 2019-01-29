72b06f18ae3909c8f1bdf9e94ea0421f462992e3
s3virge <kobzarvit@gmail.com>
2019-01-29 11:10:19 +0200

    was added status READY

--- src/main/java/app/models/DeviceStatus.java
+++ src/main/java/app/models/DeviceStatus.java
@@ -1,10 +1,11 @@
 package app.models;
 
 public class DeviceStatus {
-    public static final String giveOut     = "Выдано"; //5
-    public static final String diagnostics = "Диагностика"; //2
-    public static final String waitingForComponents = "Ожидание комплектующих"; //3
-    public static final String received    = "Принято"; //1
-    public static final String repair      = "Ремонт"; //2
+    public static final String GIVEOUT = "Выдано"; //5
+    public static final String DIAGNOSTICS = "Диагностика"; //2
+    public static final String WAITING_FOR_COMPONENTS = "Ожидание комплектующих"; //3
+    public static final String RECEIVED = "Принято"; //1
+    public static final String REPAIR = "Ремонт"; //2
+    public static final String READY = "Готово"; //2
 
 }
